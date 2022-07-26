0008246effa7b8c700feaf911785c1f90b71ee0b
Wandreus <wandreusmuhl70@gmail.com>
2022-07-26 16:25:53 -0400

    Add api conteinering

--- api/src/database/connection.js
+++ api/src/database/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize')
 require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+  host: process.env.DB_SERVICE,
   port: process.env.DB_PORT,
   dialect: process.env.DB_DIALECT
 })
